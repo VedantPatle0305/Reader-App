0a390f36263188d5c644938313ee6aaee8183432
VedantPatle0305 <vedantpatle594@gmail.com>
2025-09-15 23:40:10 +0530

    Initial Commit

---
+++ Reader App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reader App
+//
+//  Created by Vedant Patle on 15/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
